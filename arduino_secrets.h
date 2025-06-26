fbd2206d70321f44249e3e4f40e1dccb2c0bd422
robertooolhe <105889767+robertoolhe@users.noreply.github.com>
2025-06-26 11:54:32 -0300

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,4 +1,2 @@
-#define SECRET_SSID "Desktop_F9016920"  // your network SSID (name)
-#define SECRET_PASS "Senha123"  	// your network password (use for WPA, or use as key for WEP)
-#define NEON_PROXY "api.us-east-2.aws.neon.tech" // e.g. "api.eu-central-1.aws.neon.tech" 
-#define DATABASE_URL "postgresql://dbpainel_owner:npg_RyMKN4FIzX1P@ep-red-sea-a54m4dgs-pooler.us-east-2.aws.neon.tech/dbpainel?sslmode=require" // exemplo: "postgresql://neondb_owner:yourpassword@ep-quiet-wildcat-a2l67iq9-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=requir"
+#define SECRET_SSID "COLOQUE AQUI SEU SSIDE DE REDE WIFI"  // your network SSID (name)
+#define SECRET_PASS "COLOQUE AQUI SUA SENHA DO WIFI"  	// your network password (use for WPA, or use as key for WEP)
