5303b792fc34757323c193b896de76acfebd6965
robertooolhe <105889767+robertoolhe@users.noreply.github.com>
2025-06-27 14:10:33 -0300

    Delete arduino_secrets.h

--- arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-//SALVE O arduino_secrets.h na pasta \src
-#define SECRET_SSID "COLOQUE AQUI SEU SSIDE DE REDE WIFI"  // your network SSID (name)
-#define SECRET_PASS "COLOQUE AQUI SUA SENHA DO WIFI"  	// your network password (use for WPA, or use as key for WEP)
